7361fb206267f0409d5277902b773edb4ac48c58
shrawan <shrawan29yadav@gmail.com>
2025-08-19 17:49:19 +0530

    UI library mota mota

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,63 @@
 
-import './App.css'
-import { Button } from './components/Ui/Button'
-
+import './App.css';
+import { Button } from './components/Ui/Button';
+import { PlusIcon } from './icons/plusicon';
+import { ShareIcon } from './icons/ShareIcon';
 function App() {
 
   return (
     <>
-    <Button/>
+      <Button
+        variant={'primary'}
+        starticon={<PlusIcon size={'lg'} />}
+        endIcon={<ShareIcon size={'lg'} />}
+        size='lg'
+        title={"share"}
+       ></Button>
+
+       <Button
+        variant={'secondary'}
+        starticon={<PlusIcon size={'lg'} />}
+        endIcon={<ShareIcon size={'lg'} />}
+        size='lg'
+        title={"share"}
+       ></Button>
+
+       <Button
+        variant={'primary'}
+        starticon={<PlusIcon size={'lg'} />}
+        endIcon={<ShareIcon size={'lg'} />}
+        size='md'
+        title={"share"}
+       ></Button>
+
+       
+
+       <Button
+        variant={'secondary'}
+        starticon={<PlusIcon size={'lg'} />}
+        endIcon={<ShareIcon size={'lg'} />}
+        size='md'
+        title={"share"}
+       ></Button>
+
+       <Button
+        variant={'primary'}
+        starticon={<PlusIcon size={'md'} />}
+        endIcon={<ShareIcon size={'md'} />}
+        size='sm'
+        title={"share"}
+       ></Button>
+
+       <Button
+        variant={'secondary'}
+        starticon={<PlusIcon size={'sm'} />}
+        endIcon={<ShareIcon size={'sm'} />}
+        size='sm'
+        title={"share"}
+       ></Button>
+
+
     </>
   )
 }
--- src/components/Ui/Button.tsx
+++ src/components/Ui/Button.tsx
@@ -1,15 +1,36 @@
-interface ButtonProps{
-    variant :"primary"| "secondary";
-    size:"sm" | "md" | "lg";
-    text :string;
-    stratIcon? : any;
-    endIcon?:any;
-    onClick :()=>void
+import type { ReactElement } from "react";
 
+interface propsInterface {
+    title: string | ReactElement;
+    size: 'lg' | 'md' | 'sm';
+    starticon?: ReactElement;
+    endIcon?: ReactElement
+    variant:"primary"|"secondary"
 }
 
-export const Button = (props : ButtonProps )=>{
-    return <Button></Button>
-}
+const sizeStyles = {
+    "lg": "px-8 py-4 text-xl rounded-xl",
+    "md": "px-4 py-2 text-md rounded-md",
+    "sm": "px-2 py-1 text-sm rounded-sm"
+};
 
-<Button variant="primary" size="md" onClick={()=>{}} text="asasass" />
+const variantStyle = {
+  "primary": "bg-purple-600 text-white",     // Primary style = purple background + white text
+  "secondary": "bg-purple-200 text-purple-600" // Secondary style = light purple background + dark purple text
+};
+
+
+export function Button(props: propsInterface) {
+    return <button className={sizeStyles[props.size] + " "+ variantStyle[props.variant]} >
+        <div className="flex item-center" >
+
+            {props.starticon}
+            <div className="pl-2 pr-2">
+            {props.title}</div>
+            {props.endIcon}
+
+        </div>
+    </button>
+
+
+}
